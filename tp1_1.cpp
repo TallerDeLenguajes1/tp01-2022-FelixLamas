69962a4c6ea7730db3d7ca3c02612a8e9fbb6866
Felix Lamas <felamas@gmail.com>
2022-07-11 18:25:17 -0300

    se resuelve el punto a) al f)

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,11 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hola mundo");
+    int a=10, *puntero;
+    puntero = &a;
+    printf("EL contenido del punteto es %p \n", puntero);
+    printf("La direccion de memoria de la variable es: %p \n", &a);
+    printf("La direccion de memoria del puntero es: %p \n", &puntero);
+    printf("El tamaño de memoria utilizado por la variable es: %d \n", sizeof(a));
     return 0;
 }
