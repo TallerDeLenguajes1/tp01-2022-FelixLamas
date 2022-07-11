b3aa837bf8b3de6d33521d5caf1b577c04d2297f
Felix Lamas <felamas@gmail.com>
2022-07-11 20:07:39 -0300

    funcion void, muestra la variable y direccion de memoria

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -2,9 +2,11 @@
 // declaracion de funciones
 int cuadrado(int num);
 void cuadradoV(int num2, int *p);
+void mostrarVariable(int *p_var);
 
 int main(){
     int cuad, cuad2, *puntero, numero1, numero2;
+    int variable, *p_var;
     printf("Ingrese un numero entero:");
     scanf("%d", &numero1);
     cuad = cuadrado(numero1);
@@ -14,6 +16,10 @@ int main(){
     puntero = &cuad2;
     cuadradoV(numero2, puntero);
     printf("El cuadrado del numero %d es %d", numero2, cuad2);
+    printf("Ingrese un numero entero:");
+    scanf("%d", &variable);
+    p_var = &variable;
+    mostrarVariable(p_var);
     return 0;
 }
 
@@ -23,4 +29,8 @@ int cuadrado(int num){
 
 void cuadradoV(int num2, int *p){
     *p = num2 * num2;
+}
+
+void mostrarVariable(int *p_var){
+    printf("La variable ingresada es %d y la direccion de memoria de la variable es: %p", *p_var, p_var);
 }
