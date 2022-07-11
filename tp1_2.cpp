857006f7003d5059893c398109e9eade60fb58ed
Felix Lamas <felamas@gmail.com>
2022-07-11 19:58:12 -0300

    funcion cuadrado de tipo void

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -1,15 +1,26 @@
 #include<stdio.h>
 // declaracion de funciones
 int cuadrado(int num);
+void cuadradoV(int num2, int *p);
+
 int main(){
-    int cuad, numero;
+    int cuad, cuad2, *puntero, numero1, numero2;
     printf("Ingrese un numero entero:");
-    scanf("%d", &numero);
-    cuad = cuadrado(numero);
-    printf("El cuadrado del numero %d es %d", numero, cuad);
+    scanf("%d", &numero1);
+    cuad = cuadrado(numero1);
+    printf("El cuadrado del numero %d es %d", numero1, cuad);
+    printf("Ingrese un numero entero:");
+    scanf("%d", &numero2);
+    puntero = &cuad2;
+    cuadradoV(numero2, puntero);
+    printf("El cuadrado del numero %d es %d", numero2, cuad2);
     return 0;
 }
 
 int cuadrado(int num){
     return num * num;
+}
+
+void cuadradoV(int num2, int *p){
+    *p = num2 * num2;
 }
