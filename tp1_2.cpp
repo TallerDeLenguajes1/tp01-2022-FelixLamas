619d0acc73300dfaa0879231deee70a8a5ebe9bf
Felix Lamas <felamas@gmail.com>
2022-07-12 21:34:21 -0300

    funcion para invertir la posicion de enteros

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -3,23 +3,31 @@
 int cuadrado(int num);
 void cuadradoV(int num2, int *p);
 void mostrarVariable(int *p_var);
+void invertir(int *a, int *b);
 
 int main(){
     int cuad, cuad2, *puntero, numero1, numero2;
-    int variable, *p_var;
+    int variable, *p_var, a, b;
     printf("Ingrese un numero entero:");
     scanf("%d", &numero1);
     cuad = cuadrado(numero1);
-    printf("El cuadrado del numero %d es %d", numero1, cuad);
-    printf("Ingrese un numero entero:");
+    printf("\nEl cuadrado del numero %d es %d", numero1, cuad);
+    printf("\nIngrese un numero entero:");
     scanf("%d", &numero2);
     puntero = &cuad2;
     cuadradoV(numero2, puntero);
-    printf("El cuadrado del numero %d es %d", numero2, cuad2);
-    printf("Ingrese un numero entero:");
+    printf("\nEl cuadrado del numero %d es %d", numero2, cuad2);
+    printf("\nIngrese un numero entero:");
     scanf("%d", &variable);
     p_var = &variable;
     mostrarVariable(p_var);
+    printf("\nIngrese el primer numero entero:");
+    scanf("%d", &a);
+    printf("\nIngrese el segundo numero entero:");
+    scanf("%d", &b);
+    printf("\nlos numeros ingresados son %d y %d", a, b);
+    invertir(&a, &b);
+    printf("\nlos numeros invertidos son %d y %d", a, b);
     return 0;
 }
 
@@ -33,4 +41,11 @@ void cuadradoV(int num2, int *p){
 
 void mostrarVariable(int *p_var){
     printf("La variable ingresada es %d y la direccion de memoria de la variable es: %p", *p_var, p_var);
+}
+
+void invertir(int *a, int *b){
+    int aux;
+    aux = *a;
+    *a = *b;
+    *b = aux;
 }
